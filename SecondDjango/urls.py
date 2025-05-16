a20eea00e2983ce9a6f279327929c20c7d13ed1a
Belutkina Elena <belenka@rambler.ru>
2025-05-16 15:54:07 +0300

    M-3-All

--- SecondDjango/urls.py
+++ SecondDjango/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('item/<int:item_id>/', views.show_item, name='show_item'),
     path('items/', views.item_list, name='item_list'),
+    
 ]
