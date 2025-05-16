8b0413c499fea03e2ead24f561d2fa5d002fac04
Belutkina Elena <belenka@rambler.ru>
2025-05-16 16:46:11 +0300

    M-3:part2:add product description from DB

--- MyApp/models.py
+++ MyApp/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Item(models.Model):
    name  = models.CharField(max_length=100)
    brand = models.CharField(max_length=100)
+   description = models.TextField(blank=True, null=True)
    count = models.PositiveIntegerField() 
  
