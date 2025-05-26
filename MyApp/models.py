28249a5d7f210c8330995af7c45424f815b041dc
Belutkina Elena <belenka@rambler.ru>
2025-05-26 13:22:13 +0300

    Connecting css and editing color

--- MyApp/models.py
+++ MyApp/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
+class Color(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Item(models.Model):
    name  = models.CharField(max_length=100)
    brand = models.CharField(max_length=100)
    description = models.TextField(blank=True, null=True)
    count = models.PositiveIntegerField() 
+   colors = models.ManyToManyField(Color, related_name='items', blank=True)
+   
+   def __str__(self):
+        return f"{self.name}, {self.brand}"
